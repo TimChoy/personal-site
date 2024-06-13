c4a3c34225cbd345d1d726b176c99d9970b8c268
Tim Choy <timchoy99@gmail.com>
2024-06-12 20:14:02 -0400

    Add tabs to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,11 @@
 import React from "react"
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, Box, Container, Avatar, Tabs, Tab } from "@mui/material";
+import styles from "./navbar.module.css";
 
 export default function NavBar() {
   return (
     <AppBar 
+      // className={styles.navbar1}
       style={{
         width: "93.5%",
         top: "2%",
@@ -13,11 +15,64 @@ export default function NavBar() {
         background: "rgb(0,0,0)",
       }}
     >
-      <Toolbar>
-        <Typography>
-          Tim Choy
-        </Typography>
-      </Toolbar>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontWeight: 700,
+              color: "inherit",
+              textDecoration: "none",
+            }}
+            className="cursorp"
+          >
+            <Avatar
+              sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+              className="cursorp Tab8 animate__animated animate__backInLeft"
+            >
+              TC
+            </Avatar>
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <>
+              <Tabs centered sx={{ margin: "auto" }}>
+                <Tab
+                  value="one"
+                  label={
+                    <p>
+                      Home
+                    </p>
+                  }
+                  className={`Tab1 animate__animated animate__zoomIn ${styles.navTabs}`}
+                />
+
+                <Tab 
+                  label={
+                    <p>
+                      About Me
+                    </p>
+                  }
+                  className={`Tab2 animate__animated animate__zoomIn ${styles.navTabs}`}
+                />
+                
+                <Tab 
+                  label={
+                    <p>
+                      My Projects
+                    </p>
+                  }
+                  className={`Tab3 animate__animated animate__zoomIn ${styles.navTabs}`}
+                />
+              </Tabs>
+            </>
+          </Box>
+        </Toolbar>
+      </Container>
     </AppBar>
   )
 }
