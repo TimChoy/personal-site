2a5f45a7a1b02c848d791d67204ccc20e8500a1e
Tim Choy <timchoy99@gmail.com>
2025-01-06 16:52:54 -0500

    Add resume and mobile navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
-"use client"
+"use client";
 import { TypeAnimation } from "react-type-animation";
 import styles from "./page.module.css";
 import EmailIcon from "@mui/icons-material/Email";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import { Tooltip } from "@mui/material";
 
 export default function Home() {
   return (
     <div className={styles.main}>
       <TypeAnimation
         className={styles.typeAnimation}
-        sequence = {[
+        sequence={[
           "Hello!",
           2000,
           "Bonjour!",
@@ -30,19 +32,28 @@ export default function Home() {
       <div className={styles.homeTitle}>
         Welcome to my website! I&apos;m Timothy Choy
       </div>
-      <p>
-        {"Reach out to me here:"}
-      </p>
+      <p>Reach out to me here:</p>
       <div className={styles.icons}>
-        <a href={`mailto:timchoy99@gmail.com`} target="_blank">
-          <EmailIcon />
-        </a>
-        <a href={`https://www.linkedin.com/in/tim-choy/`} target="_blank">
-          <LinkedInIcon />
-        </a>
+        <Tooltip title="Email">
+          <a href={`mailto:timchoy99@gmail.com`} target="_blank">
+            <EmailIcon />
+          </a>
+        </Tooltip>
+        <Tooltip title="Linkedin">
+          <a href={`https://www.linkedin.com/in/tim-choy/`} target="_blank">
+            <LinkedInIcon />
+          </a>
+        </Tooltip>
+        <Tooltip title="Github">
         <a href={`https://github.com/TimChoy`} target="_blank">
           <GitHubIcon />
         </a>
+        </Tooltip>
+        <Tooltip title="Resume">
+          <a href="/Timothy-Choy-Resume.pdf" target="_blank">
+            <InsertDriveFileIcon />
+          </a>
+        </Tooltip>
       </div>
     </div>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,41 @@
-'use client'
+"use client";
 
-import React, { useState } from "react"
-import { AppBar, Toolbar, Box, Container, Tabs, Tab } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  Container,
+  Tabs,
+  Tab,
+  IconButton,
+} from "@mui/material";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
 import styles from "./navbar.module.css";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function NavBar() {
   const [selectedTab, setSelectedTab] = useState("one");
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const router = useRouter();
+
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = (path: string | null) => {
+    setAnchorEl(null);
+    if (path) {
+      router.push(path);
+    }
+  };
 
   return (
     <React.Fragment>
-      <AppBar 
+      <AppBar
         style={{
           width: "75%",
           top: "2%",
@@ -22,43 +47,70 @@ export default function NavBar() {
       >
         <Container maxWidth="xl">
           <Toolbar disableGutters>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" }}}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ display: { xs: "flex", md: "none" } }}
+              onClick={handleClick}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="dropdown-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={() => handleClose(null)}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              slotProps={{
+                paper: {
+                  style: {
+                    background: "black",
+                    color: "white",
+                    minWidth: "50%",
+                  },
+                },
+              }}
+            >
+              <MenuItem onClick={() => handleClose("/")}>Home</MenuItem>
+              <MenuItem onClick={() => handleClose("/about")}>About Me</MenuItem>
+              <MenuItem onClick={() => handleClose("/projects")}>My Projects</MenuItem>
+            </Menu>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
               <>
                 <Tabs centered sx={{ margin: "auto" }} value={selectedTab}>
                   <Tab
                     value="one"
                     label={
-                      <Link
-                        href="/"
-                        className={styles.linkTabs}
-                      >
-                        <p>
-                          Home
-                        </p>
+                      <Link href="/" className={styles.linkTabs}>
+                        <p>Home</p>
                       </Link>
                     }
                     className={`Tab1 animate__animated animate__zoomIn ${styles.navTabs}`}
                   />
 
-                  <Tab 
+                  <Tab
                     value="two"
                     label={
                       <Link href="/about" className={styles.linkTabs}>
-                        <p>
-                          About Me
-                        </p>
+                        <p>About Me</p>
                       </Link>
                     }
                     className={`Tab2 animate__animated animate__zoomIn ${styles.navTabs}`}
                   />
-                  
+
                   <Tab
                     value="three"
                     label={
                       <Link href="/projects" className={styles.linkTabs}>
-                        <p>
-                          My Projects
-                        </p>
+                        <p>My Projects</p>
                       </Link>
                     }
                     className={`Tab3 animate__animated animate__zoomIn ${styles.navTabs}`}
@@ -70,5 +122,5 @@ export default function NavBar() {
         </Container>
       </AppBar>
     </React.Fragment>
-  )
+  );
 }
